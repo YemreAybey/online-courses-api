6366451c4c231295930e1145e9131b8c7b3d3119
Yemre <aybey77@gmail.com>
2019-12-04 14:29:08 +0300

    change of seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@ require 'faker'
 
 TYPE = ['Javascript', 'Ruby', 'Rails', 'React']
 Course.destroy_all
+User.destroy_all
+Favourite.destroy_all
 10.times do |ind|
   Course.create({
     author: Faker::ProgrammingLanguage.creator,
